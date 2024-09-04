a3d278c6c3658523dbe4fefe85b6d906154a08c5
Hector3009 <56938999+Hector3009@users.noreply.github.com>
2024-09-04 11:09:12 -0600

    Se agregan los proyectos de negocio, infraestructura y el microservicio de Choferes

---
+++ Gen9-Parent2024/Gen9-Negocio2024/Gen9-Choferes2024/src/test/java/com/hernandez/choferes/Gen9Choferes2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hernandez.choferes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gen9Choferes2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
